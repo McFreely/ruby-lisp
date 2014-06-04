91e32382625aca5c421d4022bf80596fc957a063
Paul McFreely <paulmcfreely@gmail.com>
2014-06-04 12:10:30 +0200

    Creates code for part 1

---
+++ parser.rb
@@ -0,0 +1,110 @@
+# This is the parser module, with the 'parse' function
+# which you'll implement as part 1 of the workshop.
+# Its job is to convert strings into data structures that
+# the evaluator can understand
+
+def parse(source)
+	# Parse string representation of one *single*
+	# expression into the corresponding Abstract Syntax Tree
+
+	raise NotImplementedError, "Do It Yourself"
+end
+
+# Below are a few useful utility functions. These should
+# come in handy when implementing 'parse'. We don't
+# want to spend the day implementing parenthesis counting
+# after all.
+
+def remove_comments(source)
+	# Remove from a string anything in between a ; and a linebreak
+	source.sub(/;.*\n/, "\n")
+end
+
+def find_matching_paren(source, start = 0)
+	# Given a string and the index of an opening paren,
+	# determines the index of the matching closing paren.
+	raise "The program is not valid Lisp" unless source[start] == "("
+	pos = start
+	open_parens = 1
+	while open_parens > 0 do
+		pos += 1
+		if source.length == pos 
+			raise ArgumentError, 'Incomplete Expression'
+		elsif source[pos] == "("
+			open_parens += 1
+		elsif source[pos] == ")"
+			open_parens -= 1
+		end
+	end
+	pos
+end
+
+def split_exps(source)
+	# Splits a source string into subexpressions
+	# that can be parsed individually.
+	source = source.strip
+	exps = []
+	while rest
+		exp, rest = first_expression(rest)
+		exps.app(exp)
+	end
+	exps
+end
+
+def first_expression(source)
+	# Split string into (exp, rest) where exp is the
+	# first expression in the string and rest is the 
+	# rest of the string after this expression.
+	source = source.strip
+	if source[0] == "'"
+		exp, rest = first_expression(source[1..-1])
+		return source[0] + exp, rest
+	elsif source[0] == "("
+		last = find_matching_paren(source)
+		return source[0..last + 1], source[last + 1..-1]
+	else
+		match = /^[^\s)']+/.match(source) # some renaming to do here
+		ending = match.end(0)
+		atom = source[0..ending]
+		return atom, source[ending..-1]
+	end
+end
+
+
+# The functions below, 'parse_multiple' and 'unparse' are
+# implemented in order for the REPL to work. Don't worry
+# about them when implementing the language.
+
+def parse_multiple(source)
+	# Create a list of ASTs from program source constituting
+	# multiple expressions.
+	source = remove_comments(source)
+	split_exps(source).each do |exp|
+		parse(exp)
+	end
+end
+
+def unparse(ast)
+	# Turn an AST back into lisp program source
+	if is_boolean(ast)
+		ast ? '#t' : '#f'
+	elsif is_list(ast)
+		if ast.length > 0 && ast[0] == "quote"
+			return "'%s" % [unparse(ast[1])]
+		else
+			return "(%s)" % [ast.each {|x| x.join(" ")}]
+		end
+	else
+		ast.to_s
+	end 
+end
+
+
+# To move in own file when refactoring
+def is_boolean(ast)
+	ast.instance_of? String
+end
+
+def is_list(ast)
+	ast.instance_of? Array
+end
---
+++ spec/parse_spec.rb
@@ -0,0 +1,134 @@
+require 'spec_helper'
+require './parser'
+
+describe "Parsing" do
+	it "should parse a single symbol" do
+		# Symbols are represented by text strings.
+		# Parsing a single atom should result in an AST consisting of only that symbol
+
+		expect(parse("foo")).to eq("foo")
+	end
+
+	it "should parse a single boolean" do
+		# Booleans are the special symbols #t and #f
+		# In the ASTs they are represented by Ruby true and false, respectively
+
+		expect(parse('#t')).to eq(true)
+		expect(parse('#f')).to eq(false)
+	end
+
+	it "should parse a single integer" do
+		# Integers are represented in the ASTs as Ruby ints
+
+		expect(parse('42')).to eq(42)
+		expect(parse('1337')).to eq(1337)
+	end
+
+	it "should parse a list of only symbols" do
+		# A list is reoresented by a number of elements surrounded by parens
+		# Ruby arrays are used to represent  lists as ASTs
+		#
+		# Tip: the useful helper function 'find_matching_paren' is already provided
+		# in 'parser.rb'
+
+		list = %w(foo bar list)
+		empty_list = []
+		expect(parse('(foo bar baz)')).to eq(list)
+		expect(parse('()')).to eq(empty_list)
+	end
+
+	it "should parse a list of mixed types" do
+		# When parsing lists, make sure each of the sub-expressions are also parsed properly
+
+		expect(parse('(foo #t 123)')).to eq(%w(foo true 123))
+	end
+
+	it "should parse a snested list" do
+		program = '(foo (bar ((#t)) x) (baz y))'
+		ast = ['foo',
+		       ['bar', [[true]], 'x'],
+		       ['baz', 'y']]
+		expect(parse(program)).to eq(ast)
+	end
+
+	it "should raise an execption if any paren is missing" do
+		program = "(foo (bar x y)"
+
+		expect(parse(program)).to raise_error(ArgumentError)
+	end
+
+	it "should raise an execption if there is too much parens" do
+		# The parse function expects to receive onlyu one single expression
+		# Anything more than this, should result in the proper exception
+		program = "(foo (bar x y)))"
+
+		expect(parse(program)).to raise_error(ArgumentError)
+	end
+
+	it "should remove excess whitespace" do
+		program = "(program   with      much   whitespace  )      "
+		ast = %w{program with much whitespace}
+
+		expect(parse(program)).to eq(ast)
+	end
+
+	it "should strip comments" do
+		program = " ;the first line is a comment
+					     (define variable
+					     		;another comment
+					     		if #t
+					     			 42 ;inline comment!
+					     			 (something else)))"
+
+		ast = ["define", "variable",
+										 ["if", true,
+										 	 42,
+										 	 ["something", "else"]]]
+
+	  expect(parse(program)).to eq(ast)
+	end
+
+	it "should be able to parse a large input" do
+		program = " (define fact
+									;; Factorial function
+									(lambda (n)
+										  (if (<= n 1)
+										  	  1 ;Factorial of 0 is 1, and we deny
+										  	    ;the existence of negative numbers
+										  	  (* n (fact (- n 1))))))"
+					    
+    ast = ['define', 'fact',
+                    ['lambda', ['n'],
+                     ['if', ['<=', 'n', '1'],
+                      1,
+                      ['*', 'n', ["fact", ["-", "n", 1]]]]]]
+
+    expect(parse(program)).to eq(ast)
+  end
+
+
+  #
+  # The following tests checks that quote expansion works properly
+  # 
+
+  it "should extend the shorthand quote syntax" do
+  	# Quoting is a shorthand syntax for calling the quote form
+  	# Examples:
+  	#
+  	# 		`foo       -> (quote foo)
+  	#     `(foo bar) -> (quote (foo bar))
+  	program = "(foo `nil)"
+  	ast = ["foo", ["quote", "nil"]]
+
+  	expect(parse(program)).to eq(ast)
+  end
+
+  it 'should expand nested quotes' do
+  	program = "````foo"
+  	ast = ["quote", ["quote", ["quote", ["quote", "foo"]]]]
+
+  	expect(parse(program)).to eq(ast)
+  end
+end
+
+
