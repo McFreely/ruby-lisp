b1ed81868750a8199fa963b72711225a4089670d
Paul McFreely <paulmcfreely@gmail.com>
2014-06-07 23:32:02 +0200

    Creates code for part 2

---
+++ evaluator.rb
@@ -0,0 +1,16 @@
+require_relative 'types'
+require_relative 'ast'
+require_relative 'asserts'
+require_relative 'parser'
+
+
+# This is the Evaluator module. The 'evaluate' function is the heart
+# of your language, and the focus for most of parts 2 through 6
+#
+# A socre of usefule functions is provided for you, as per the above imports,
+# making your work a bit easier. (We're supposed to get through this thing in a day,
+# after all.)
+
+def evaluate(ast, env)
+	raise NotImplementedError, "Do It Yourself"
+end
