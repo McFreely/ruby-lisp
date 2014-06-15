fa2823025462ff7e17e3869ffd29aaae36bca382
Paul McFreely <paulmcfreely@gmail.com>
2014-06-15 17:10:50 +0200

    Fixes typos and a few code error for part 2

--- ast.rb
+++ ast.rb
@@ -1,28 +1,28 @@
 require_relative 'types'
 
 # This module contains a few simple helper functions
-# for checking the type os as_trees
+# for checking the type of as_trees
 
 def is_symbol?(as_tree)
-	as_tree.instance_of? String
+	as_tree.kind_of? String
 end
 
 def is_list?(as_tree)
-	as_tree.instance_of? Array
+	as_tree.kind_of? Array
 end
 
 def is_boolean?(as_tree)
-	as_tree.instance_of?(TrueClass) || as_tree.instance_of?(FalseClass)
+	as_tree.kind_of?(TrueClass) || as_tree.kind_of?(FalseClass)
 end
 
 def is_integer?(as_tree)
-	as_tree.instance_of? Int
+	as_tree.kind_of? Integer
 end
 
 def is_closure?(as_tree)
-	as_tree.instance_of? Closure
+	as_tree.kind_of? Closure
 end
 
 def is_atom?(as_tree)
-	return is_symbol? || is_integer? || is_boolean? || is_closure
+	return is_symbol?(as_tree) || is_integer?(as_tree) || is_boolean?(as_tree) || is_closure?(as_tree)
 end	
